949ed478949ec4a78cda4246c486e2d35065a094
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2021-03-30 17:24:29 +0200

    add new components

---
+++ src/pages/homepage/Homepage..js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Directory from '../../components/directory/Directory';
+
+import './HomepageStyle.scss';
+/**
+* @author Milos Tanaskovic
+* @function HomePage
+**/
+
+const HomePage = (props) => {
+  return(
+   <div className='homepage'>
+    <Directory />
+   </div>
+   )
+  }
+
+
+export default HomePage
